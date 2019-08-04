3e8d70122b6d4a1012fffb53e39003c6a0a31b0d
mohamednajiub <m.emad550@gmail.com>
2019-08-04 21:21:54 +0200

    add currencies select

--- src/components/Navigation/Information/Information.jsx
+++ src/components/Navigation/Information/Information.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import classes from './Information.module.scss';
 import {Data} from '../../../Data';
+import Select from 'react-select';
 
 class Information extends Component{
+    state = {
+        selectOption: null,
+        currencies: [
+            {
+                label: 'EGP',
+                value: 'egp'
+            },
+            {
+                label: 'USD',
+                value: 'usd'
+            },
+            {
+                label: 'EUR',
+                value: 'eur'
+            }
+        ]
+    }
+    handleChange = selectedOption => {
+        this.setState({ selectedOption });
+        console.log(`Option selected:`, selectedOption);
+    };
+    
     render(){
         return(
             <header className={classes.InformationHeader}>
                 <img src={Data.paymentMethods.source} />
+                <Select
+                    value={this.state.selectedOption}
+                    onChange={this.handleChange}
+                    options={this.state.currencies}
+                />
             </header>
         )
     }
