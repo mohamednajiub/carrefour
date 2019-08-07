b4aec0f2679f320d4c9d6d29039a900425bf6fcb
mohamednajiub <m.emad550@gmail.com>
2019-08-07 07:00:00 +0200

    Updates

---
+++ precache-manifest.6b7ed0b1da88718a3fe07eb16072066b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b91c671660d0006cdf6fe599142552",
+    "url": "/carrefour/index.html"
+  },
+  {
+    "revision": "88719b0fc965eaab3f54",
+    "url": "/carrefour/static/css/2.87c40933.chunk.css"
+  },
+  {
+    "revision": "2283f9a72c99a4b7afb1",
+    "url": "/carrefour/static/css/main.dafbceb8.chunk.css"
+  },
+  {
+    "revision": "88719b0fc965eaab3f54",
+    "url": "/carrefour/static/js/2.43078be9.chunk.js"
+  },
+  {
+    "revision": "2283f9a72c99a4b7afb1",
+    "url": "/carrefour/static/js/main.e5fc0d76.chunk.js"
+  },
+  {
+    "revision": "27c1c48160f6cc27ca80",
+    "url": "/carrefour/static/js/runtime~main.b544e2d8.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/carrefour/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/carrefour/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/carrefour/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/carrefour/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- src/components/Logos/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import * as classes from './Logo.module.scss';
-const coloredLogo = (props)=>(
-    <div className={classes.Logo} style={{height: props.height}}>
-        <img src={props.source} alt={props.name}/>
-    </div>
-);
-
-export default coloredLogo;
