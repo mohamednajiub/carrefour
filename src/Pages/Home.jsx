2c9a3c53bf5163239bf70878b04412fa4fd86088
mohamednajiub <m.emad550@gmail.com>
2019-08-08 19:42:10 +0200

    Feat: add pages container and add Home Page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Carousel from '../components/Carousel/Carousel'
+import Categories from '../components/Categories/Categories';
+import Branches from '../components/Branches/Branches';
+import ProductsCollections from '../components/ProductsCollections/ProductsCollections';
+import Isotope from '../components/Isotope/Isotope';
+import * as Data from '../Data'
+
+const home = () => {
+  return (
+    <div>
+        <Carousel />
+        <Categories />
+        <Branches />
+        <Isotope componentTitle="hot deals" componentSubtitle="new hot deals now" Data={Data.HotDeals}/>
+        <ProductsCollections />
+        <Isotope componentTitle="our products" componentSubtitle="Browse our fascinating sections!" Data={Data.HotDeals}/>
+    </div>
+  );
+}
+
+export default home;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import './App.module.scss';
 import Layout from '../hoc/Layout/Layout';
-import Carousel from '../components/Carousel/Carousel'
+import Home from '../Pages/Home';
+import { Route } from 'react-router-dom';
 
-import Categories from '../components/Categories/Categories';
-import Branches from '../components/Branches/Branches';
-import HotDeals from '../components/HotDeals/HotDeals';
-import ProductsCollections from '../components/ProductsCollections/ProductsCollections';
-import Isotope from '../components/Isotope/Isotope';
-import * as Data from '../Data'
-import Footer from '../components/Footer/Footer';
-
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Layout>
-        <Carousel />
-        <Categories />
-        <Branches />
-        <Isotope componentTitle="hot deals" componentSubtitle="new hot deals now" Data={Data.HotDeals}/>
-        <ProductsCollections />
-        <Isotope componentTitle="our products" componentSubtitle="Browse our fascinating sections!" Data={Data.HotDeals}/>
-
+        <Route path="/" component={Home} exact/>
       </Layout>
     </div>
   );
