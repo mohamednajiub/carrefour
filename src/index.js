510e9800b4d51653eebcff38b4272cdf0b26e71c
mohamednajiub <m.emad550@gmail.com>
2019-08-11 15:59:49 +0200

    use hash router instead of browser router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter  } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter >
         <App />
-    </BrowserRouter>
+    </HashRouter>
     , document.getElementById('root')
 );
 
